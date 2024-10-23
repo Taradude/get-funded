3adfe1c94a4e3bc736e4d65f4eb87483e3b0373e
Taradude <taraha2012@gmail.com>
2024-10-23 22:59:11 +0300

    bg

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="animated-gradient">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss">
+body {
+  padding: 0;
+  margin: 0;
+}
+.animated-gradient {
+  margin: 0;
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(-66deg, #000000, #2d1075, #0f0b5f, #000000);
+  background-size: 250% 250%;
+  overflow: hidden;
+  animation: gradient-animation 16s ease infinite;
+}
+
+@keyframes gradient-animation {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,28 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import MainLayout from '@/layouts/MainLayout.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: '/',
+    component: MainLayout,
+    children: [
+      {
+        path: '/about',
+        name: 'about',
+        component: () => import('../views/AboutView.vue'),
+      },
+      {
+        path: '/home',
+        name: 'HomeView',
+        component: () => import('@/views/HomeView.vue'),
+      },
+    ],
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-];
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
-});
+})
 
-export default router;
+export default router
